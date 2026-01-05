bc1709873400145c626f9853acf69b2d2d3f64ec
David Plicka <PlickaDavid@litv.sssvt.cz>
2026-01-05 09:04:50 +0100

    feat: created project by template

---
+++ src/FoodDeliveryWebApplication/FoodDeliveryWebApplication.WebMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodDeliveryWebApplication.WebMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
