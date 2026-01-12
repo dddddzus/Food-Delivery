b4f4dece462c8f1e79e0729b69f23af928856869
David Plicka <PlickaDavid@litv.sssvt.cz>
2026-01-12 09:41:02 +0100

    chore: DbContext

---
+++ src/FoodDeliveryWebApplication/FoodDeliveryWebApplication.WebMvcApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FoodDeliveryWebApplication.WebMvcApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<xxxx> xxxxs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=4c1_plickadavid_db2;user=plickadavid;password=123456");
+        }
+    }
+}
